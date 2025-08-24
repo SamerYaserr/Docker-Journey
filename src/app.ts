68ae3bc140b27d1b2ce1541b510ec525181e56db
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-25 01:32:22 +0300

    add redis image

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import express from "express";
 import { Request, Response } from "express";
+import { createClient } from "redis";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) =>
-  res.send("<h1> Hello Samora!</h1>")
-);
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
 
+const redisClient = createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.connect();
+
+app.get("/", async (req: Request, res: Response) => {
+  await redisClient.set("reply", "Hello from Redis!");
+  res.send("<h1> Hello Samora!</h1>");
+});
+
+app.get("/data", async (req: Request, res: Response) => {
+  let message: string = "<h1> Hello Samora!</h1>";
+  await redisClient.get("reply").then((reply) => {
+    message = message + "<h2>" + reply + "</h2>";
+  });
+
+  res.send(message);
+});
 export default app;
