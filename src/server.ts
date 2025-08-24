3cf226079d5f3c530dca6f96bef8321d5f3ffa74
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-24 17:55:49 +0300

    add mongo image

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,13 @@ import env from "./config/env.config";
 
 const PORT = env.PORT || 3000;
 
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+
+const URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
 const server = app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
